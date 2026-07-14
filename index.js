e4861e2e67f6f1b4c8e1791ef5dcf0a952d2b1df
Tommy Grullon <tommycontreras34@gmail.com>
2026-07-14 10:40:01 -0400

    Implement command handling for movie types in index.js

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
+import { config } from "./config/index.js"
+import { fetchMoviesCurrentlyPlaying, fetchPopularMovies, fetchTopRatedMovies, fetchUpcomingMovies } from "./helper.js"
+
+config.validateEnvVariables()
+
 process.stdin.on("data", (data) => {
     const input = data.toString().trim().split(" ")
 
     const command = input[0]
+    const arg = input.slice(1)
 
     switch(command) {
         case "tmdb-app":
-            console.log("TMDB APP")
+            const typeProperty = arg[0]
+            if(typeProperty != "--type") {
+                console.log("You must specify a type of movie, like this --type (playing, popular, top or upcoming)")
+                return
+            }
+            
+            const typeValue = arg[1].replaceAll('"', "")
+
+            const movieHandlers = {
+              playing: fetchMoviesCurrentlyPlaying,
+              popular: fetchPopularMovies,
+              top: fetchTopRatedMovies,
+              upcoming: fetchUpcomingMovies,
+            };
+
+            const handler = movieHandlers[typeValue]
+
+            if (!handler) {
+              console.error("Invalid movie type.");
+            } else {
+              handler();
+            }
 
             break
         case "exit":

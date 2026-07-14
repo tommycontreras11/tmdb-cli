a176e3d1c55128283cfb234505a67bb58fe9c16c
Tommy Grullon <tommycontreras34@gmail.com>
2026-07-14 14:47:53 -0400

    Refactor movie fetching logic to improve type validation and error handling

--- helper.js
+++ helper.js
@@ -1,5 +1,14 @@
 import { config } from "./config/index.js";
 
+const API_RESOURCE_PATH = "3/movie";
+
+const movieTypeToEndpoint = new Map([
+  ["playing", "now_playing"],
+  ["popular", "popular"],
+  ["top", "top_rated"],
+  ["upcoming", "upcoming"],
+]);
+
 const validateResponse = (response, data) => {
   if (!response.ok) {
     throw new Error(data.status_message);
@@ -10,26 +19,41 @@ const validateResponse = (response, data) => {
   }
 };
 
-const API_RESOURCE_PATH = "3/movie";
-
-export const fetchMoviesBasedOnType = async (type) => {
-  try {
-    const response = await fetch(
-      `${config.TMDB_API_URL}/${API_RESOURCE_PATH}/${type}?language=en-US&page=1`,
-      {
-        headers: {
-          "Content-type": "Application/json",
-          Authorization: `Bearer ${config.TMDB_API_KEY}`,
-        },
-      },
-    );
-
-    const data = await response.json();
-
-    validateResponse(response, data);
-
-    console.log(data);
-  } catch (error) {
-    console.log(error.message);
+export const getMovieEndpoint = (arg) => {
+  const typeProperty = arg[0];
+  if (typeProperty != "--type") {
+    throw new Error("You must specify the type property, like this --type");
   }
+
+  const typeValue = arg[1]?.replaceAll('"', "");
+  if (!typeValue) {
+    throw new Error(
+      "You must specify a type of movie, like this --type (playing, popular, top or upcoming)",
+    );
+  }
+
+  const endpoint = movieTypeToEndpoint.get(typeValue);
+
+  if (!endpoint) {
+    throw new Error(
+      `Invalid movie type. Choose one of: ${[...movieTypeToEndpoint.keys()].join(", ")}`,
+    );
+  }
+  return endpoint;
+};
+
+export const fetchMovies = async (endpoint) => {
+  const response = await fetch(
+    `${config.TMDB_API_URL}/${API_RESOURCE_PATH}/${endpoint}?language=en-US&page=1`,
+    {
+      headers: {
+        Authorization: `Bearer ${config.TMDB_API_KEY}`,
+      },
+    },
+  );
+
+  const data = await response.json();
+
+  validateResponse(response, data);
+  return data;
 };
--- index.js
+++ index.js
@@ -1,44 +1,28 @@
-import { config } from "./config/index.js"
-import { fetchMoviesBasedOnType } from "./helper.js"
+import { config } from "./config/index.js";
+import { getMovieEndpoint, fetchMovies } from "./helper.js";
 
-config.validateEnvVariables()
-
-const movieType = new Map([
-    ["playing", "now_playing"],
-    ["popular", "popular"],
-    ["top", "top_rated"],
-    ["upcoming", "upcoming"]
-])
+config.validateEnvVariables();
 
 process.stdin.on("data", async (data) => {
-    const input = data.toString().trim().split(" ")
+  const input = data.toString().trim().split(" ");
 
-    const command = input[0]
-    const arg = input.slice(1)
+  const command = input[0];
+  const arg = input.slice(1);
 
-    switch(command) {
-        case "tmdb-app":
-            const typeProperty = arg[0]
-            if(typeProperty != "--type") {
-                console.log("You must specify a type of movie, like this --type (playing, popular, top or upcoming)")
-                return
-            }
-            
-            const typeValue = arg[1].replaceAll('"', "")
+  switch (command) {
+    case "tmdb-app":
+      try {
+        const endpoint = getMovieEndpoint(arg);
 
-            const endpoint = movieType.get(typeValue);
-
-            if (!endpoint) {
-              console.error("Invalid movie type. Choose: playing, popular, top, or upcoming.");
-              return;
-            }
-            
-            await fetchMoviesBasedOnType(endpoint);
-
-            break
-        case "exit":
-            process.exit()
-        default:
-            console.log("Command not valid.")
-    }
-})
+        const movies = await fetchMovies(endpoint);
+        console.log(movies);
+      } catch (error) {
+        console.error(error.message);
+      }
+      break;
+    case "exit":
+      process.exit();
+    default:
+      console.log("Command not valid.");
+  }
+});

88fc1469ac2ee3deb1216f9a9c3487d3ce834c5a
Tommy Grullon <tommycontreras34@gmail.com>
2026-07-14 10:39:33 -0400

    Implement command handling in index.js and update package.json to use ES modules

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello World!")
+process.stdin.on("data", (data) => {
+    const input = data.toString().trim().split(" ")
+
+    const command = input[0]
+
+    switch(command) {
+        case "tmdb-app":
+            console.log("TMDB APP")
+
+            break
+        case "exit":
+            process.exit()
+        default:
+            console.log("Command not valid.")
+    }
+})

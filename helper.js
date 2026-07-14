f32f949c60042daa5df518f22aecb2cdd7e0fa8e
Tommy Grullon <tommycontreras34@gmail.com>
2026-07-14 10:39:55 -0400

    Add movie fetching functions to helper.js

---
+++ helper.js
@@ -0,0 +1,15 @@
+export const fetchMoviesCurrentlyPlaying = () => {
+    console.log("Now playing")
+}
+
+export const fetchPopularMovies = () => {
+    console.log("Popular")
+}
+
+export const fetchTopRatedMovies = () => {
+    console.log("Top rated")
+}
+
+export const fetchUpcomingMovies = () => {
+    console.log("Upcoming movies")
+}
